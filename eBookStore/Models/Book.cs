d0896b7ce843db7835fa29ddb76cd0d49cb7e351
TanyaK <pctanya.kuo@gmail.com>
2022-10-21 11:31:18 +1100

    first commit

---
+++ eBookStore/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace eBookStore.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Reserve { get; set; }
+    }
+}
+
